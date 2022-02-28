8a567c2d4aad8dda5496cd277619930570ec21ac
I am the Carl <jamescarl96@gmail.com>
2022-02-28 14:05:53 +0000

    Induce crash directly on VoxelStreamRegionFiles.

--- godot_project/main.gd
+++ godot_project/main.gd
@@ -1,23 +1,28 @@
 extends Spatial
 
 var cycle: int = 0
-var should_fill: bool = false
+
+var voxel_stream: VoxelStreamRegionFiles = VoxelStreamRegionFiles.new()
+
+func _init():
+	var directory: Directory = Directory.new()
+	var _result: int = directory.remove("user://world/regions/lod0/r.0.0.0.vxr")
+
+	voxel_stream.directory = "user://world"
 
 func _process(_delta: float):
 	print(cycle)
 	cycle += 1
-	
-	var voxel_tool: VoxelTool = $VoxelTerrain.get_voxel_tool()
-	
-	if should_fill:
-		voxel_tool.value = 1
-	else:
-		voxel_tool.value = 0
-	
-	should_fill = !should_fill
-	
-	voxel_tool.channel = VoxelBuffer.CHANNEL_TYPE
-	voxel_tool.mode = voxel_tool.MODE_SET
-	voxel_tool.do_sphere(Vector3(0, 0, 0), 16)
-	
-	$VoxelTerrain.save_modified_blocks()
+
+	var buffer: VoxelBuffer = VoxelBuffer.new()
+
+	# Put something in there.
+	buffer.create(16, 16, 16)
+
+	# Put something interesting in there.
+	for x in buffer.get_size_x() - 1:
+		for y in buffer.get_size_y() - 1:
+			for z in buffer.get_size_z():
+				buffer.set_voxel(randi() % 256, x, y, z)
+
+	voxel_stream.immerge_block(buffer, Vector3(cycle, 0, 0), 0)
